81eb7777d4c62068db57daf883e7e65e20c6a937
esejosue <josuegallardolara@hotmail.com>
2022-10-26 18:24:22 -0700

    Explanation of models

--- inventory/models.py
+++ inventory/models.py
@@ -2,3 +2,13 @@ from enum import unique
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
+#once your model is done, go to the views.py app where you want this info to be share with. 
+#This case, jump into the views.py inside the pos file
+
+# Create your models here.
+class Product(models.Model){
+    product_name=models.CharField(max_length=200, unique=True)
+    
+}
+
+
